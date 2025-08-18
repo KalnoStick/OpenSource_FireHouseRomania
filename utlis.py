903a0180d0e2c03471e06b802c83676bb7553ff1
KalnoStick <leonard.berbecaru08@gmail.com>
2025-08-18 11:05:19 +0300

    Update utlis.py

--- utlis.py
+++ utlis.py
@@ -1,9 +1,1 @@
-import sys
-import os
 
-def resource_path(relative_path):
-    """Get absolute path to resource, works for dev and PyInstaller .exe"""
-    #base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    full_path= os.path.join(base_path, relative_path)
-    return os.path.normpath(full_path)
